0e3d48d3d1af2fd4006fb17ba135c7ea68a09dc5
Petr Gregor <petr.gregor@email.cz>
2024-11-13 20:15:42 +0100

    Genre - Meta class.

--- viewer/models.py
+++ viewer/models.py
@@ -5,6 +5,9 @@ from django.db.models import Model, CharField
 class Genre(Model):
     name = CharField(max_length=32, null=False, blank=False, unique=True)
 
+    class Meta:
+        ordering = ['name']
+
     def __repr__(self):
         return f"Genre(name={self.name})"
 
