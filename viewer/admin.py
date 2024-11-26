9c52d42edea690ee8ae2178a5f7612ad7a896b91
Petr Gregor <petr.gregor@email.cz>
2024-11-26 19:36:26 +0100

    MovieAdmin.

--- viewer/admin.py
+++ viewer/admin.py
@@ -1,8 +1,70 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin
 
 from viewer.models import *
+
+
+class MovieAdmin(ModelAdmin):
+
+    @staticmethod
+    def cleanup_description(modeladmin, request, queryset):
+        for obj in queryset:
+            obj.description = ""
+            obj.save()
+
+    ordering = ['title_orig', 'year']  # ['year', 'id']
+    list_display = ['id', 'title_orig', 'title_cz', 'year']
+    list_display_links = ['id', 'title_orig']
+    list_per_page = 20
+    list_filter = ['genres', 'countries']
+    search_fields = ['title_orig', 'title_cz']
+    actions = ['cleanup_description']
+
+    fieldsets = [
+        ('Titles',
+         {'fields':
+             [
+                 'title_orig',
+                 'title_cz'
+             ]
+         }
+         ),
+        ('External information',
+         {'fields':
+              [
+                  'genres',
+                  'countries',
+                  'year',
+                  'length'
+               ],
+          'description': 'These fields are going to be filled with data parsed from external databases.'
+          }
+         ),
+        ('Creators',
+         {'fields':
+              ['directors',
+               'actors'
+               ],
+          'description': 'Information about creators.'
+          }),
+        ('User information',
+         {'fields':
+              ['description']
+          }
+         ),
+        ('Internal information',
+         {'fields':
+             [
+                 'created',
+                 'updated'
+             ]
+         }
+         )
+    ]
+    readonly_fields = ['created', 'updated']
+
 
 admin.site.register(Country)
 admin.site.register(Creator)
 admin.site.register(Genre)
-admin.site.register(Movie)
+admin.site.register(Movie, MovieAdmin)
