7222d50c4ee709b7be9ec8e9eebc8cb65b8495c4
Petr Gregor <petr.gregor@email.cz>
2024-12-03 21:01:10 +0100

    HTTP error codes templates.

--- hollymovies/settings.py
+++ hollymovies/settings.py
@@ -27,9 +27,9 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY", default="")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
